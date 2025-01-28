7501e78f9a011f940fe66d095cc96e833f400ecc
AlbinFranzen <albinfranzen@hotmail.com>
2025-01-27 16:57:59 -0800

    Page class complete

--- lstore/page.py
+++ lstore/page.py
@@ -1,14 +1,12 @@
-
 class Page:
-
     def __init__(self):
         self.num_records = 0
         self.data = bytearray(4096)
 
     def has_capacity(self):
-        pass
+        return (self.num_records < 512) # If fewer records than 512, we have capacity to write
 
-    def write(self, value):
+    def write(self, value): # Assume value is a byte
         self.num_records += 1
+        self.data[self.num_records] = value  # Write value to the next available slot
         pass
-
