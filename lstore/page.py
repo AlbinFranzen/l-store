5a9cc7f93dcfef8d4ee4cf637a46461f5713f218
AlbinFranzen <albinfranzen@hotmail.com>
2025-01-27 17:02:00 -0800

    updated order of adding record vs updating num_records

--- lstore/page.py
+++ lstore/page.py
@@ -7,6 +7,6 @@ class Page:
         return (self.num_records < 512) # If fewer records than 512, we have capacity to write
 
     def write(self, value): # Assume value is a byte
-        self.num_records += 1
         self.data[self.num_records] = value  # Write value to the next available slot
+        self.num_records += 1
         pass
