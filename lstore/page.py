25852d1954ab5e203e9c5a549f00b895862178d0
AlbinFranzen <albinfranzen@hotmail.com>
2025-01-28 11:13:08 -0800

    added overwrite function for indirection column

--- lstore/page.py
+++ lstore/page.py
@@ -6,10 +6,14 @@ class Page:
     def has_capacity(self):
         return (self.num_records < 512) # If fewer records than 512, we have capacity to write
 
-    def write(self, value): # Assume value is 8 bytes (64-bit integer)
+    def append(self, value): # Assume value is 8 bytes (64-bit integer)
        self.data[self.num_records * 8 : self.num_records * 8 + 8] = value  # Write value to the next available slot
        self.num_records += 1
        pass
+    
+    def overwrite(self, index, value): # Assume value is 8 bytes (64-bit integer)
+        self.data[index * 8 : index * 8 + 8] = value  # Overwrite value at index
+        pass
     
     def read_all(self):
         return self.data
