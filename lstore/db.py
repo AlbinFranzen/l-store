f02fcb3a61b64a5d02b7686b8bb153f1e8f294a5
yzffff666 <1367016356@qq.com>
2025-02-24 23:33:58 -0800

    wrote open and close

--- lstore/db.py
+++ lstore/db.py
@@ -1,4 +1,6 @@
 from lstore.table import Table
+import os
+from bufferpool import BufferPool
 
 class Database():
 
@@ -8,13 +10,56 @@ class Database():
         """
         self.tables = {}          # A dictionary of tables objects
         self.table_directory = {} # A dictionary of table metadata
-        pass
+        self.db_path = None       # Path to database root folder
+        self.bufferpool = None    # Bufferpool object
 
     # Not required for milestone1
     def open(self, path):
+        # Set database path
+        self.db_path = path
+        # If database path does not exist, create it
+        if not os.path.exists(path):
+            os.makedirs(path, exist_ok=True)
+        
+        # Enumerate through all tables in the database
+        for table_name in os.listdir(path):
+            table_dir = os.path.join(path, table_name)
+            if os.path.isdir(table_dir):
+                metadata_path = os.path.join(table_dir, "metadata.txt")
+                if os.path.exists(metadata_path):
+                    with open(metadata_path, 'r') as f:
+                        lines = f.readlines()
+                    # Key index is the first column, num_columns is the second column
+                    if len(lines) >= 2:
+                        key = int(lines[0].strip())
+                        num_columns = int(lines[1].strip())
+                        # Create new table
+                        table = Table(table_name, num_columns, key)
+                        # Add table to tables dictionary
+                        self.tables[table_name] = table
+                        # Add table to table_directory dictionary
+                        self.table_directory[table_name] = {
+                            "name": table_name,
+                            "num_columns": num_columns,
+                            "key_index": key
+                        }
         pass
 
     def close(self):
+        for table in self.tables.values():
+            # Enumerate through all pages in the table
+            for page_path, frame in table.bufferpool.frames.items():
+                # Write dirty page to disk
+                if frame.dirty_bit:
+                    table.bufferpool.write_to_disk(page_path, frame.page)
+                    # Reset dirty bit
+                    frame.clear_dirty_bit()
+
+            # Write metadata to disk
+            metadata_path = os.path.join(table.path, "metadata.txt")
+            with open(metadata_path, 'w') as f:
+                # Key index is the first column, num_columns is the second column
+                f.write(f"{table.key}\n{table.num_columns}\n")
         pass
 
     """
