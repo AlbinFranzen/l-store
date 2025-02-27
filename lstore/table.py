2d0f5a32a5ed64c592f7af55e9eb699491826a44
Benicyoo <benicio.bailey@gmail.com>
2025-02-26 16:50:25 -0800

    trying to fix files

--- lstore/table.py
+++ lstore/table.py
@@ -34,13 +34,14 @@ class Table:
     :param key: int             #Index of table key in columns
     """
 
-    def __init__(self, name, num_columns, key, database=None):
+    def __init__(self, name, num_columns, key, db_path):
         # Table metadata
         self.name = name
         self.key = key
         self.num_columns = num_columns
-        self.database = database
-        self.path = os.path.join("database", name)
+        self.path = os.path.join(db_path,"_tables", name)
+        self.archival_path = os.path.join(db_path, "_archives", name + "_archived")
+        self.pr_unmerged_updates = [0]  # Unmerged updates per page range
         self.page_directory = {}
         self.index = Index(self)
         self.bufferpool = BufferPool(self.path)
@@ -330,11 +331,6 @@ class Table:
     def merge(self, pagerange_index):
         # Start the merge process in a new thread
         with self.merge_lock:
-            if self.merge_lock:
-                #archive before merge process
-                if self.database:
-                    self.database.save_archive(self.name)
-                    
             if self.merge_thread is None or not self.merge_thread.is_alive():
                 self.merge_thread = threading.Thread(target=self._merge, args=(pagerange_index,))
                 self.unmerged_updates = 0
